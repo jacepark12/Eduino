ea1c2d21f1de00fca4c18d6b163e3bcc0bf66eb5
김동우 <andandend2847@gmail.com>
2016-09-08 16:24:32 +0900

    우리껄로만들

--- app.js
+++ app.js
@@ -9,19 +9,18 @@ var users = require('./routes/users.js');
 var creators = require('./routes/creators.js');
 var contents = require('./routes/contents.js');
 
-mongoose.connect('mongodb://localhost/kactale');
+mongoose.connect('mongodb://localhost/webduino');
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-  console.log('데이터베이스에 연결되었습니다.');
+  console.log('[DB]Connected!]');
 });
 
-app.use('/users', users);
-app.use('/creators', creators);
-app.use('/contents', contents);
+app.use('/users', users); //user 부분
+app.use('/project', contents); //project 부분
 
 var server = app.listen(3001, function() {
-  console.log("칵테일 Restful API 서버가 시작되었습니다.");
+  console.log("server ON");
 });
