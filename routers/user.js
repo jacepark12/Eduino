cf39fc95c8290067c5e009c5e80f23c0a5a2da5d
박재성 <jspark141515@gmail.com>
2016-09-10 11:16:11 +0900

    Added path.resolve for all sendfile functions

--- routers/user.js
+++ routers/user.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var path = require('path');
+var app = express();
 var user = express.Router();
 var userController = require('../controllers/userController.js');
 var bodyParser = require('body-parser');
@@ -17,16 +19,22 @@ user.get("/", function(req, res){
 //이부분 뭔 기능인지 잘 모르겠다_동우
 user.get("/signup", function(req, res){
   if (req.param('step') == 'terms') {
-    res.sendfile('../public/signup.html');
+    //res.sendfile('../public/signup.html');
+    res.sendfile(path.resolve(__dirname+'/../public/signup.html'));
   } else if (req.param('step') == 'form') {
-    res.sendfile('../public/signup_form.html');
+    //res.sendfile('../public/signup_form.html');
+    res.sendfile(path.resolve(__dirname+'/../public/signup_form.html'));
   } else {
-    res.send("<script> location.href = '/signup?step=terms' </script>");
+    res.send("<script> location.href = '/user/signup?step=terms' </script>");
   }
 });
 
 user.get("/session", function(req, res){
   res.send(req.session);
+});
+
+user.get("/signin", function(req, res){
+  res.sendFile('../public/signin.html');
 });
 
 user.post("/signin", function(req, res){
