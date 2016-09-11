1e0d59d36f5e97277166a981d5f57f73476aa97b
김동우 <andandend2847@gmail.com>
2016-09-11 17:25:14 +0900

    projectController 구조만 잡아둠

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -6,5 +6,13 @@ var model = require('../models/projectModel.js');
 
 module.exports = {
 
+  getProject : function(req, res){
+    var sid = req.session.sid;
+    //session 에 있는 아이디의 프로젝트를 불러와서 렌더해서 보내주는 부분을 만들려 합니다
+  },
+
+  save : function(req, res, id){
+    //이건 프로젝트 저장부분  id는 프로젝트 id임
+  }
 
 }
