de55378a179759fc4776fd68959255db9a9e9739
김동우 <andandend2847@gmail.com>
2016-09-12 08:24:05 +0900

    Add createdDate, modifiedDate to projectModel

--- models/projectModel.js
+++ models/projectModel.js
@@ -6,6 +6,8 @@ var projectSchema = new Schema({
   contents: String, //설명같은거 들어갈듯....
   xml: String, // 프로젝트 관련 xml 들어갈듯
   owner: String // 프로젝트 소유자의 이메일
+  createdDate: Date,
+  modifiedDate: Date
 });
 
 module.exports = mongoose.model('project', projectSchema);
