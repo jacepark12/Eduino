cd41b2bcf44ed3c8f32fb83e58ff0aca90c2ba93
박재성 <jspark141515@gmail.com>
2016-09-12 13:54:26 +0900

    Fixed Syntax error in projectModel.js

--- models/projectModel.js
+++ models/projectModel.js
@@ -5,7 +5,7 @@ var projectSchema = new Schema({
   projectname: String,
   contents: String, //설명같은거 들어갈듯....
   xml: String, // 프로젝트 관련 xml 들어갈듯
-  owner: String // 프로젝트 소유자의 이메일
+  owner: String, // 프로젝트 소유자의 이메일
   createdDate: Date,
   modifiedDate: Date
 });
