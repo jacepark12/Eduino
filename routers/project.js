d56011577648591bf01129a2e1fb9ed781438f25
김동우 <andandend2847@gmail.com>
2016-09-13 10:48:05 +0900

    Edit button color (red->blue), 일부 주석 수

--- routers/project.js
+++ routers/project.js
@@ -18,8 +18,8 @@ project.post('/save/:id', function(req, res){
 });
 
 project.get('/add', function(req, res){
-  //TODO req.session.email 없으면 예외처리 해주장
-  //Controller로 뺄까? ㄴㄴ 일단 여기다
+  //req.session.email 없으면 예외처리 해주장 -> 해결함
+  //Controller로 뺄까? -> ㄴㄴ 일단 여기다
   var email = req.session.email;
   if (email){ //세션, 이메일이 있는경우
     res.render('createProject', {url : "test"});
