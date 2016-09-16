c80cb54183b3c9e7c30a1eee208b28d373882bdb
박재성 <jspark141515@gmail.com>
2016-09-16 19:29:01 +0900

    Changed contents -> description

--- models/projectModel.js
+++ models/projectModel.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
   projectname: String,
-  contents: String, //설명같은거 들어갈듯....
+  description: String, //설명같은거 들어갈듯....
   xml: String, // 프로젝트 관련 xml 들어갈듯
   owner: String, // 프로젝트 소유자의 이메일
   createdDate: Date,
